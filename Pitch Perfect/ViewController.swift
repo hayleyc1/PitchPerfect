8a3f3ff37b259f6ef9cf9d3b1469f5d66b3ea3ae
Hayley Cundiff <hcsparklegirl@gmail.com>
2015-11-13 15:23:20 -0700

    Started project

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+
+    @IBOutlet weak var RecordinginProgress: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+     func stopAudio(sender: UIButton) {
+        RecordinginProgress.hidden = true
+        
+    }
 
+     func recordAudio(sender: UIButton) {
+        RecordinginProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.enabled = false
+        
+        
+        //TODO: Record the user's voice
+        println("in recordAudio")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        //make stop button disappear
+        stopButton.hidden = true
+        recordButton.enabled = true 
+    }
 }
-
