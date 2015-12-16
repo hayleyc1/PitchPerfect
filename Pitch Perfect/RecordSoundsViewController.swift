04ab18f7b69b40e8453fbf4974a5afea0cd11022
Hayley Cundiff <hcsparklegirl@gmail.com>
2015-12-15 21:41:34 -0700

    Did a lot of stuff

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecordSoundsViewController.swift
 //  Pitch Perfect
 //
 //  Created by Hayley Cundiff on 11/13/15.
@@ -7,14 +7,14 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import AVFoundation
+class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
-
     @IBOutlet weak var RecordinginProgress: UILabel!
+    var audioRecorder:AVAudioRecorder!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -29,13 +29,34 @@ class ViewController: UIViewController {
 
      func stopAudio(sender: UIButton) {
         RecordinginProgress.hidden = true
-        
+        let audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
     }
 
      func recordAudio(sender: UIButton) {
         RecordinginProgress.hidden = false
         stopButton.hidden = false
         recordButton.enabled = false
+        
+        //Declared Globally
+        var audioRecorder:AVAudioRecorder!
+        
+        //Inside func recordAudio(sender: UIButton)
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+       
+        let recordingName = "my_audio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        audioRecorder = AVAudioRecorder(URL: filePath!, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
         
         
         //TODO: Record the user's voice
